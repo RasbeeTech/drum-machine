c89b04de2168d32fb1591e890b33b313c3cd965e
MykalB <mykalb081@gmail.com>
2021-06-04 16:52:38 -0400

    init
    - Setup view
    - Replace icon
    - Code clean up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      sample: ''
+    };
+  }
+  render(){
+    return (
+      <div className="container-fluid">
+        <h2 className='text-center'>Drum Machine</h2>
+        <div id='drum-machine' className='text-center'>
+          <div id='display'>
+          </div>
+          <button id='' className='drum-pad'>Q</button>
+          <button id='' className='drum-pad'>W</button>
+          <button id='' className='drum-pad'>E</button>
+          <br />
+          <button id='' className='drum-pad'>A</button>
+          <button id='' className='drum-pad'>S</button>
+          <button id='' className='drum-pad'>D</button>
+          <br />
+          <button id='' className='drum-pad'>Z</button>
+          <button id='' className='drum-pad'>X</button>
+          <button id='' className='drum-pad'>C</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
