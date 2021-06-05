89a5d37ab3807f20df6db9778ff54a763c1620c3
MykalB <mykalb081@gmail.com>
2021-06-05 14:50:35 -0400

    Update App.js
    - code refactoring/ clean up

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import React from 'react';
 
+
 const APPNAME = 'Drum Machine'
-const DESCR = [
-  'flat-kick-drum',
-  'steel-drum',
-  'big-kick-drum',
-  'african-wood-drum',
-  'roller-drum',
-  'lofi-drum',
-  'electric-drum',
-  'deep-drum',
-  'talking-drum'
-];
-const URLS = [
-  'https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/STYLE%20KITS%20(Hip%20Hop,%20Etc)/Khezie%20Hip%20Hop%20Kit/74[kb]khezie-Flat-Kick-Drum.wav.mp3',
-  'https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/African%20and%20Eastern%20Percussion/83[kb]steeldrum.wav.mp3',
-  'https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Kicks/53[kb]big-v-kick-drum.wav.mp3',
-  'https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/ETHNIC%20and%20WORLD%20PERCUSSION/African%20Wooden%20Drum/103[kb]African_Wooden_Drum_08_High_02.wav.mp3',
-  'https://sampleswap.org/samples-ghost/DRUM%20LOOPS%20and%20BREAKS/161%20to%20180%20bpm/130[kb]163_rollerdrum.wav.mp3',
-  'https://sampleswap.org/samples-ghost/DRUM%20LOOPS%20and%20BREAKS/131%20to%20140%20bpm/611[kb]137_lofi-cheesy-drum-machine.wav.mp3',
-  'https://sampleswap.org/samples-ghost/DRUM%20LOOPS%20and%20BREAKS/121%20to%20130%20bpm/348[kb]126_silly-electrodrums.wav.mp3',
-  'https://sampleswap.org/samples-ghost/DRUM%20LOOPS%20and%20BREAKS/111%20to%20120%20bpm/344[kb]120_deep-drum-click.wav.mp3',
-  'https://sampleswap.org/samples-ghost/DRUM%20LOOPS%20and%20BREAKS/111%20to%20120%20bpm/182[kb]118_talking_drum.wav.mp3'
+const AUDIO = [
+  {id: 'flat-kick-drum', url: 'https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/STYLE%20KITS%20(Hip%20Hop,%20Etc)/Khezie%20Hip%20Hop%20Kit/74[kb]khezie-Flat-Kick-Drum.wav.mp3'},
+  {id: 'steel-drum', url: 'https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/African%20and%20Eastern%20Percussion/83[kb]steeldrum.wav.mp3'},
+  {id: 'big-kick-drum', url: 'https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Kicks/53[kb]big-v-kick-drum.wav.mp3'},
+  {id: 'african-wood-drum', url: 'https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/ETHNIC%20and%20WORLD%20PERCUSSION/African%20Wooden%20Drum/103[kb]African_Wooden_Drum_08_High_02.wav.mp3'},
+  {id: 'roller-drum', url: 'https://sampleswap.org/samples-ghost/DRUM%20LOOPS%20and%20BREAKS/161%20to%20180%20bpm/130[kb]163_rollerdrum.wav.mp3'},
+  {id: 'lofi-drum', url: 'https://sampleswap.org/samples-ghost/DRUM%20LOOPS%20and%20BREAKS/131%20to%20140%20bpm/611[kb]137_lofi-cheesy-drum-machine.wav.mp3'},
+  {id: 'electric-drum', url: 'https://sampleswap.org/samples-ghost/DRUM%20LOOPS%20and%20BREAKS/121%20to%20130%20bpm/348[kb]126_silly-electrodrums.wav.mp3'},
+  {id: 'deep-drum', url: 'https://sampleswap.org/samples-ghost/DRUM%20LOOPS%20and%20BREAKS/111%20to%20120%20bpm/344[kb]120_deep-drum-click.wav.mp3'},
+  {id: 'talking-drum', url: 'https://sampleswap.org/samples-ghost/DRUM%20LOOPS%20and%20BREAKS/111%20to%20120%20bpm/182[kb]118_talking_drum.wav.mp3'}
 ];
 
 class App extends React.Component {
@@ -29,34 +19,43 @@ class App extends React.Component {
     super(props);
     this.state = {
       keys: ['Q','W','E','A','S','D','Z','X','C'],
-      ids: DESCR,
-      urls: URLS
+      ids: AUDIO.map(x => x.id),
+      urls: AUDIO.map(x => x.url),
+      desc: ''
     };
     this.play = this.play.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
   }
+
   componentDidMount() {
     document.addEventListener("keydown", this.handleKeyPress);
   }
+
   componentWillUnmount() {
     document.removeEventListener("keydown", this.handleKeyPress);
   }
+
   handleKeyPress(event){
     let char = String.fromCharCode(event.keyCode)
-    this.play(char);
+    let desc = this.state.ids[this.state.keys.indexOf(char)]
+    this.play(char, desc);
   }
-  play(id){
-    console.log(this.state.keys);
+
+  play(id, desc){
+    this.setState({
+      desc: desc
+    });
     if(this.state.keys.includes(id)){
       let audio = document.getElementById(id);
       audio.play();
     }
   }
+
   render(){
     let drumPads = this.state.keys.map((key, index) => {
       return(
-        <button id={this.state.ids[index]} className='drum-pad' onClick={() => this.play(key)}>
-          {key}{index}
+        <button id={this.state.ids[index]} className='drum-pad' onClick={() => this.play(key, this.state.ids[index])}>
+          {key}
           <audio id={key} className='clip' src={this.state.urls[index]}></audio>
         </button>
       );
@@ -65,8 +64,7 @@ class App extends React.Component {
       <div className="container-fluid">
         <h2 className='text-center'>{APPNAME}</h2>
         <div id='drum-machine' className='text-center'>
-          <div id='display'>
-          </div>
+          <div id='display' className='bg-danger'>{this.state.desc}</div>
           {drumPads}
         </div>
       </div>
